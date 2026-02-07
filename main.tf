f7a6f498a0a1cc340f31f26183ba21852353968d
Mahi Begoug <51762779+MahiBegoug@users.noreply.github.com>
2026-02-07 13:58:10 -0500

    test an instance

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "google" {
 }
 
 module "kubernetes" {
-  source = "./modules/kubernetes-k8s-agents-machine-data-extraction"
+  source = "./modules/kubernetes-k8s-agents-machine-data-extraction-data"
 
   master_sa_email             = var.master_sa_email
   master_sa_scopes            = var.master_sa_scopes
