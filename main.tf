21f224e41b54d5d468de45778d2740f69f9c0344
Mahi Begoug <51762779+MahiBegoug@users.noreply.github.com>
2026-02-07 12:13:44 -0500

    fix temperature

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "google" {
 }
 
 module "kubernetes" {
-  source = "./modules/kubernetes-k8s-agents-machine-data"
+  source = "./modules/kubernetes-k8s-agents-machine-data-extraction"
 
   master_sa_email             = var.master_sa_email
   master_sa_scopes            = var.master_sa_scopes
